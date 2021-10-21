670c14b0daa8c17c3acf2c3cdf1415f3eda6e93f
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-10-21 16:35:39 +0100

    Update datasets to src_ prefix

--- cnz/models/staging/nsul/stg_nsul__all.sql
+++ cnz/models/staging/nsul/stg_nsul__all.sql
@@ -2,11 +2,11 @@
     config(
         schema="nsul"
     )
-}} 
+}}
 
 select
     dataset_date,
     uprn,
     lad21cd as local_authority_district_code
 
-from {{ source("raw_nsul", "nsul") }}
+from {{ source("nsul", "nsul") }}
