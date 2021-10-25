7d8a34c0fcb35b38369dfefaefa8bfc2e49bc48f
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-10-25 11:00:57 +0100

    Fix clean up script (#6)

--- scripts/dbt_pull_request_closed.sh
+++ scripts/dbt_pull_request_closed.sh
@@ -7,6 +7,13 @@ if [[ -z $GITHUB_SHA ]] ; then
     exit 1
 fi
 
-bq ls --project_id "$DEV_PROJECT_ID" --format=json \
-    | jq --arg SCHEMA "ci_${GITHUB_SHA::7}" '.[] | .id | select(. | contains($SCHEMA))' \
+DBT_SCHEMA="ci_${GITHUB_SHA::7}"
+
+# on first call bq prints a welcome message to stdout that interferes with jq
+bq ls --headless --project_id "$DEV_PROJECT_ID" 1> /dev/null
+
+echo "==> Deleting resources under schema $DBT_SCHEMA..."
+
+bq ls --headless --project_id "$DEV_PROJECT_ID" --format=json \
+    | jq --arg DBT_SCHEMA "$DBT_SCHEMA" '.[] | .id | select(. | contains($DBT_SCHEMA))' \
     | xargs -t -n1 bq rm -r -f --dataset
