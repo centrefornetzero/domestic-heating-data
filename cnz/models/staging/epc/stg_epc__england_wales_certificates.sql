4965e5ed4d0a727ed6f32c490e69b84fb0e323fb
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-10-27 19:37:51 +0100

    Fix line lengths

--- cnz/models/staging/epc/stg_epc__england_wales_certificates.sql
+++ cnz/models/staging/epc/stg_epc__england_wales_certificates.sql
@@ -40,7 +40,8 @@ final as (
         contains_substr(main_heat_description, 'boiler') as has_boiler,
         contains_substr(main_heat_description, 'warm air') as has_warm_air,
         contains_substr(main_heat_description, 'storage heaters') as has_storage_heaters,
-        contains_substr(main_heat_description, 'room heaters') or contains_substr(main_heat_description, 'gwresogyddion ystafell') as has_room_heaters,
+        contains_substr(main_heat_description, 'room heaters')
+            or contains_substr(main_heat_description, 'gwresogyddion ystafell') as has_room_heaters,
         contains_substr(main_heat_description, 'community scheme') as has_district_heating,
         contains_substr(main_heat_description, 'heat pump') as has_heat_pump,
         regexp_extract(main_heat_description, "(\\w+) source heat pump") as heat_pump_source,
@@ -49,8 +50,10 @@ final as (
 
         -- secondary heating
         {{ get_heating_fuel('secondary_heat_description') }} as secondary_heating_fuel,
-        contains_substr(secondary_heat_description, 'room heaters') or contains_substr(secondary_heat_description, 'gwresogyddion ystafell') as has_secondary_room_heaters,
-        contains_substr(secondary_heat_description, 'portable heaters') or contains_substr(secondary_heat_description, 'cludadwy') con as has_secondary_portable_heaters,
+        contains_substr(secondary_heat_description, 'room heaters')
+            or contains_substr(secondary_heat_description, 'gwresogyddion ystafell') as has_secondary_room_heaters,
+        contains_substr(secondary_heat_description, 'portable heaters')
+            or contains_substr(secondary_heat_description, 'cludadwy') as has_secondary_portable_heaters,
 
         -- hot water
         contains_substr(hot_water_description, 'from main system') as has_hot_water_from_heating_system,
