19868707eff5dc6ce89b2f4d9f8dbc8709dbdfeb
Tom Phillips <tom.phillips@centrefornetzero.org>
2021-10-27 20:00:37 +0100

    Replace macro with simpler Jinja loop

--- cnz/models/staging/epc/stg_epc__england_wales_certificates.sql
+++ cnz/models/staging/epc/stg_epc__england_wales_certificates.sql
@@ -35,13 +35,29 @@ final as (
     select
         *,
 
+        -- fuels
+        {% set systems = ["main", "secondary"] %}
+        {% for system in systems %}
+        case
+            when regexp_contains({{ system }}_heat_description, '\\b(mains gas|nwy prif gyflenwad)') then 'gas'
+            when contains_substr({{ system }}_heat_description, 'electric')
+                or contains_substr({{ system }}_heat_description, 'trydan') then 'electricity'
+            when regexp_contains({{ system }}_heat_description, '\\b(bottled gas|lpg)') then 'lpg'
+            when regexp_contains({{ system }}_heat_description, '\\b(oil)') then 'oil'
+            when
+                regexp_contains(
+                    {{ system }}_heat_description, '\\b(mineral|wood|coal|smokeless|anthracite|solid)'
+                ) then 'solid fuel'
+            when contains_substr({{ system }}_heat_description, 'biomass') then 'biomass'
+        end as {{ system }}_heating_fuel,
+        {% endfor %}
+
         -- main heating
-        {{ get_heating_fuel('main_heat_description') }} as main_heating_fuel,
         contains_substr(main_heat_description, 'boiler') as has_boiler,
         contains_substr(main_heat_description, 'warm air') as has_warm_air,
         contains_substr(main_heat_description, 'storage heaters') as has_storage_heaters,
         contains_substr(main_heat_description, 'room heaters')
-            or contains_substr(main_heat_description, 'gwresogyddion ystafell') as has_room_heaters,
+        or contains_substr(main_heat_description, 'gwresogyddion ystafell') as has_room_heaters,
         contains_substr(main_heat_description, 'community scheme') as has_district_heating,
         contains_substr(main_heat_description, 'heat pump') as has_heat_pump,
         regexp_extract(main_heat_description, "(\\w+) source heat pump") as heat_pump_source,
@@ -49,11 +65,10 @@ final as (
         contains_substr(main_heat_description, 'underfloor') as has_underfloor_heating,
 
         -- secondary heating
-        {{ get_heating_fuel('secondary_heat_description') }} as secondary_heating_fuel,
         contains_substr(secondary_heat_description, 'room heaters')
-            or contains_substr(secondary_heat_description, 'gwresogyddion ystafell') as has_secondary_room_heaters,
+        or contains_substr(secondary_heat_description, 'gwresogyddion ystafell') as has_secondary_room_heaters,
         contains_substr(secondary_heat_description, 'portable heaters')
-            or contains_substr(secondary_heat_description, 'cludadwy') as has_secondary_portable_heaters,
+        or contains_substr(secondary_heat_description, 'cludadwy') as has_secondary_portable_heaters,
 
         -- hot water
         contains_substr(hot_water_description, 'from main system') as has_hot_water_from_heating_system,
