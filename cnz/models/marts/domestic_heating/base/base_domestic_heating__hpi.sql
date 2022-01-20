85eba0480caf0fc53a173c97430ed783c305b829
Ryan Jenkinson <ryan.jenkinson@centrefornetzero.org>
2022-01-20 18:06:18 +0000

    Calculate `current_property_value_gbp` based on `property_type` and `built_form`
    In a given LSOA, we actually have the house price index for flats,
    detached properties etc. We always fall back to the general HPI in case
    information is not available, but adjust the HPI according to property
    type and built form to get more accurate values

--- cnz/models/marts/domestic_heating/base/base_domestic_heating__hpi.sql
+++ cnz/models/marts/domestic_heating/base/base_domestic_heating__hpi.sql
@@ -13,9 +13,15 @@ final as (
     select
         period,
         area_code,
-        first_value(index) over (partition by area_code order by period desc) / index as price_factor
+        first_value(index) over area / index as price_factor,
+        first_value(detached_index) over area / detached_index as detached_price_factor,
+        first_value(semi_detached_index) over area / semi_detached_index as semi_detached_price_factor,
+        first_value(terraced_index) over area / terraced_index as terraced_price_factor,
+        first_value(flat_index) over area / flat_index as flat_price_factor
 
     from uk_hpi
+
+    window area as (partition by area_code order by period desc)
 
 )
 
